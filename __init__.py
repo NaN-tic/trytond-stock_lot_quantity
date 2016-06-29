8cdfaae16f39af9bf1704748c2815a9ece51e248
Raimon Esteve <raimon@nan-tic.com>
2016-06-29 10:01:31 +0200

    Upgrade last changes from 3.4

--- __init__.py
+++ __init__.py
@@ -2,10 +2,15 @@
 # copyright notices and license terms.
 from trytond.pool import Pool
 from .stock import *
+from .location import *
 
 
 def register():
     Pool.register(
         Lot,
         Move,
+        LotsByLocationStart,
         module='stock_lot_quantity', type_='model')
+    Pool.register(
+        LotsByLocation,
+        module='stock_lot_quantity', type_='wizard')
